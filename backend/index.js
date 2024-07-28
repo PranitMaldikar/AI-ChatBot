3ddc707f8df25bb1ff8fcb82d27984190cb86861
Pranit Shrikrishna Maldikar <67967573+PranitMaldikar@users.noreply.github.com>
2024-07-28 17:05:58 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,6 @@ const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
 
-app.use(cors({
-  origin: 'https://ai-chat-bot-ui.vercel.app',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.options('*', cors());
-
 require('dotenv').config();
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
@@ -22,11 +14,11 @@ app.get('/ping', (req, res) => {
 });
 
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
 
 app.listen(PORT, () => {
-    console.log(Server is running on ${PORT})
+    console.log(`Server is running on ${PORT}`)
 })
